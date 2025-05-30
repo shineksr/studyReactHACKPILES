e522a954d15e331080ba9f823e588cceb458bdaa
shine <shineksr@gmail.com>
2025-05-30 23:15:40 +0530

    haii

--- src/Component/Demo.jsx
+++ src/Component/Demo.jsx
@@ -1,6 +1,11 @@
+import { Fragment } from "react";
 const Demo =()=>{
-    return <p>This is demo</p>;
-
+    return (
+        <Fragment>
+             <p>This is demo</p>
+            <p>This is second demo</p>
+        </Fragment>
+    ); 
 };
 export default Demo;
 // Compare this snippet from src/Component/Demo.jsx:
---
+++ src/Component/Todos.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Todos(){
+    <div>
+        <h1>Shavos's TOdos(sunday)</h1>
+        <ul>
+            <li>hai how are you</li>
+            <li>I am here</li>
+        </ul>
+    </div>
+}
