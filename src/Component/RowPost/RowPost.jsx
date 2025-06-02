4fe6a573b530f69d0647c1dd5005225f7fd5baea
shine <shineksr@gmail.com>
2025-06-03 00:57:42 +0530

    asdsa

--- src/Component/RowPost/RowPost.jsx
+++ src/Component/RowPost/RowPost.jsx
@@ -20,6 +20,21 @@ function RowPost() {
         <img className='poster' src="https://www.themoviedb.org/t/p/w1280/9VI8qqZgBRIu6YWnzelHBOoTdRS.jpg" alt="poster" />
         <img className='poster' src="https://www.themoviedb.org/t/p/w1280/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
         <img className='poster' src="https://www.themoviedb.org/t/p/w1280/6WxhEvFsauuACfv8HyoVX6mZKFj.jpg" alt="poster" />
+        <img className='poster' src="https://www.themoviedb.org/t/p/w1280/3PFe8tqaqMeoqBXoz3CK8gKAeKr.jpg" alt="poster" />
+        <img className='poster' src="https://www.themoviedb.org/t/p/w1280/z53D72EAOxGRqdr7KXXWp9dJiDe.jpg" alt="poster" />
+        <img className='poster' src="https://www.themoviedb.org/t/p/w1280/9VI8qqZgBRIu6YWnzelHBOoTdRS.jpg" alt="poster" />
+        <img className='poster' src="https://www.themoviedb.org/t/p/w1280/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+        <img className='poster' src="https://www.themoviedb.org/t/p/w1280/6WxhEvFsauuACfv8HyoVX6mZKFj.jpg" alt="poster" />
+        <img className='poster' src="https://www.themoviedb.org/t/p/w1280/3PFe8tqaqMeoqBXoz3CK8gKAeKr.jpg" alt="poster" />
+        <img className='poster' src="https://www.themoviedb.org/t/p/w1280/z53D72EAOxGRqdr7KXXWp9dJiDe.jpg" alt="poster" />
+        <img className='poster' src="https://www.themoviedb.org/t/p/w1280/9VI8qqZgBRIu6YWnzelHBOoTdRS.jpg" alt="poster" />
+        <img className='poster' src="https://www.themoviedb.org/t/p/w1280/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+        <img className='poster' src="https://www.themoviedb.org/t/p/w1280/6WxhEvFsauuACfv8HyoVX6mZKFj.jpg" alt="poster" />
+        <img className='poster' src="https://www.themoviedb.org/t/p/w1280/3PFe8tqaqMeoqBXoz3CK8gKAeKr.jpg" alt="poster" />
+        <img className='poster' src="https://www.themoviedb.org/t/p/w1280/z53D72EAOxGRqdr7KXXWp9dJiDe.jpg" alt="poster" />
+        <img className='poster' src="https://www.themoviedb.org/t/p/w1280/9VI8qqZgBRIu6YWnzelHBOoTdRS.jpg" alt="poster" />
+        <img className='poster' src="https://www.themoviedb.org/t/p/w1280/reEMJA1uzscCbkpeRJeTT2bjqUp.jpg" alt="poster" />
+        <img className='poster' src="https://www.themoviedb.org/t/p/w1280/6WxhEvFsauuACfv8HyoVX6mZKFj.jpg" alt="poster" />
       </div>
     </div>
   )
