cbd8e4e917eac700b83a03b1323cc3cc5f7fb701
shine <shineksr@gmail.com>
2025-06-03 00:22:00 +0530

    ddd

--- src/Component/RowPost/RowPost.jsx
+++ src/Component/RowPost/RowPost.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import "./RowPost.css"
 function RowPost() {
   return (
-    <div>
-      <h2 className='row'>Trending</h2>
+    <div className='row'>
+       <h2>Trending</h2>
       <div className='posters'>
         <img className='poster' src="https://www.themoviedb.org/t/p/w1280/3PFe8tqaqMeoqBXoz3CK8gKAeKr.jpg" alt="poster" />
         <img className='poster' src="https://www.themoviedb.org/t/p/w1280/z53D72EAOxGRqdr7KXXWp9dJiDe.jpg" alt="poster" />
